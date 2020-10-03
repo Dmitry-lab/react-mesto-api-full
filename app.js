a0aedab91b2a6b44ec1cdbbd8c801c0a6b7dfdf2
Dmitry Serpkov <dmi-serpkov@yandex.ru>
2020-10-03 17:13:07 +0300

    fix: исправить ошибку

--- app.js
+++ app.js
@@ -34,12 +34,6 @@ const limiter = rateLimit({
 });
 
 app.use(cors({ origin: true }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,DELETE,PATCH,PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
 app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
